3e8c5c3610df028cf8e98f6e3c1e7a96464e6a9e
NCSaketh <nc.saketh1@gmail.com>
2020-10-26 23:56:13 +0530

    UC1 : less distance test

--- src/test/java/com/cg/cabinvoice/CabInvoiceTest.java
+++ src/test/java/com/cg/cabinvoice/CabInvoiceTest.java
@@ -15,5 +15,11 @@ public class CabInvoiceTest {
         Assert.assertEquals(35, fare, 0.0);
     }
 
-    
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        double distance = 0.2;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
 }

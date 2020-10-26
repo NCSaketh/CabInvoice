90d6ce0a1741b819d43d54c40bb5272217f90161
NCSaketh <nc.saketh1@gmail.com>
2020-10-26 23:51:23 +0530

    UC1 : Calculates Fare, given distance and time

--- src/main/java/com/cg/cabinvoice/InvoiceGenerator.java
+++ src/main/java/com/cg/cabinvoice/InvoiceGenerator.java
@@ -1,4 +1,13 @@
 package com.cg.cabinvoice;
 
 public class InvoiceGenerator {
+
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KM = 10;
+
+    public double calculateFare(double distance, int time) {
+        double fare = distance * MINIMUM_COST_PER_KM +
+                time * COST_PER_TIME;
+        return fare>5 ? fare : 5;
+    }
 }
--- src/test/java/com/cg/cabinvoice/CabInvoiceTest.java
+++ src/test/java/com/cg/cabinvoice/CabInvoiceTest.java
@@ -1,4 +1,19 @@
 package com.cg.cabinvoice;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CabInvoiceTest {
+
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 3.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(35, fare, 0.0);
+    }
+
+    
 }
